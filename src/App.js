762f034545576c46850261331186cfeabaf472e5
aysuakbaba <aysuakbabaa@gmail.com>
2023-01-05 19:53:30 +0300

    feat(Card component): add Card components using props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import Card from './components/Card';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import girl from './assets/girl.png'
+import wedding from './assets/wedding-photography 1.png'
+import bike from './assets/bike.png'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <div className='cards'>
+
+      <Card
+        img={girl}
+        rate={5}
+        number={6}
+        country="USA"
+        text="Life lessons with Katie Zaferes"
+        price={136}
+      />
+      <Card
+        img={wedding}
+        rate={5.0}
+        number={30}
+        country="USA"
+        text="Learn wedding photograph!"
+        price={125}
+      />
+      <Card
+        img={bike}
+        rate={4.8}
+        number={2}
+        country="USA"
+        text="Group Mountain Biking"
+        price={50}
+      />
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import star from '../assets/Star 1.png'
+
+function Card(props) {
+  return (
+    <div>
+        <img src={props.img} alt='swimmer'className='main--photo'></img>
+        <div className='price'>
+            <img src={star} alt='star' className='card--star'></img>
+            <span>{props.rate}</span>
+            <span className='grey'>({props.number})·</span>
+            <span className='grey'>{props.country}</span>
+        </div>
+        <p>{props.text}</p>
+        <p><span className='bold--price'>From ${props.price}</span> / person</p>
+    </div>
+  )
+}
+
+export default Card
